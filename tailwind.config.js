7a48d440ae6d6cb68140c342644488590b76f491
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2021-06-16 18:58:05 +0600

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  mode: "jit",
+  purge: {
+    enabled: false,
+    content: ["./public/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          10: "#F9F9F9",
+          20: "#EDEDED",
+        },
+        dark: {
+          10: "#464646",
+          20: "#232323",
+        },
+        blue: {
+          10: "#1654A1",
+        },
+        green: {
+          10: "#86C76D",
+        },
+      },
+      dropShadow: {
+        10: "7px 7px 0px rgba(0, 0, 0, 0.25)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
